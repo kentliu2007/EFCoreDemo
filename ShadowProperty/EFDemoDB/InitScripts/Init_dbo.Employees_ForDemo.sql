5fcecbb75c0ecb8fcafb651b502450ed88f4c8f0
kentliu2007 <kentliu2007@qq.com>
2019-01-30 10:32:53 +0800

    Added DB project

---
+++ ShadowProperty/EFDemoDB/InitScripts/Init_dbo.Employees_ForDemo.sql
@@ -0,0 +1,4 @@
+﻿if not exists(select 1 from [dbo].[Employees] where [EmployeeCode] = 'GZ001')
+   insert into [dbo].[Employees] ([EmployeeCode], [FirstName], [LastName], [JoinDate]) values ('GZ001', 'Hello', 'Guangzhou', '2019.1.1');
+if not exists(select 1 from [dbo].[Employees] where [EmployeeCode] = 'SH007')
+   insert into [dbo].[Employees] ([EmployeeCode], [FirstName], [LastName], [JoinDate]) values ('SH007', 'Hello', 'ShangHai', '2019.1.1');
---
+++ ShadowProperty/EFDemoDB/dbo/Employees.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[Employees] (
+    [EmployeeID]   INT           IDENTITY (1, 1) NOT NULL,
+    [EmployeeCode] NVARCHAR (20) NOT NULL,
+    [FirstName]    NVARCHAR (50) NOT NULL,
+    [LastName]     NVARCHAR (50) NOT NULL,
+    [JoinDate]     SMALLDATETIME NOT NULL,
+    CONSTRAINT [PK_Employees] PRIMARY KEY NONCLUSTERED ([EmployeeID] ASC),
+    CONSTRAINT [idx_Employees_EmpCode] UNIQUE CLUSTERED ([EmployeeCode] ASC)
+);
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'系统内部自生成ID', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Employees', @level2type = N'COLUMN', @level2name = N'EmployeeID';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'HR系统自编员工号，规则：城市拼音首字母+by城市自增长数字', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Employees', @level2type = N'COLUMN', @level2name = N'EmployeeCode';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'名', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Employees', @level2type = N'COLUMN', @level2name = N'FirstName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'姓', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Employees', @level2type = N'COLUMN', @level2name = N'LastName';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'最后一次入职的日期', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'Employees', @level2type = N'COLUMN', @level2name = N'JoinDate';
+
